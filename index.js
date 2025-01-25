2606e5b812916959578f09a046e6cf2c83967e17
KHIZERBUTT07 <khizerbutt105@gmail.com>
2025-01-25 15:05:38 -0800

    createdBackend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from "dotenv";
 import authRoutes from './routers/auth.js'
+import adminRoutes from './routers/admin.js'
 
 
 const app = express()
@@ -12,11 +13,11 @@ const PORT = 4000
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
-// app.use(cors('*'))
+app.use(cors('*'))
 
 dotenv.config({ path: 'config.env' });
 
-console.log("auth secret", process.env.Auth_SECERET); // Variables are now loaded from `config.env`
+// console.log("auth secret", process.env.Auth_SECERET); // Variables are now loaded from `config.env`
 
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -38,6 +39,7 @@ app.get('/check',(req,res)=>{
     
 })
 app.use('/auth',authRoutes)
+app.use('/admin',adminRoutes)
 
 app.listen(PORT,()=>{
     console.log('SERVER IS RUNNING ON PORT ',PORT);
